6e64c0bba8d3d326fab8f587a0add30d3164eec6
Clement Wang <clementw@vmware.com>
2018-12-02 21:40:05 +0800

    first commit

---
+++ src/main/java/com/wjh/demo/activity/Config.java
@@ -0,0 +1,12 @@
+package com.wjh.demo.activity;
+
+//@Configuration
+//@PropertySource(value = {"classpath:/application.properties"})
+public class Config {
+
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource dataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+}
