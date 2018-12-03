d3d2c3f60579126554318b27520b508505b85541
clement <1208661455@qq.com>
2018-12-04 00:38:16 +0800

    temp

--- src/main/java/com/wjh/demo/activity/AppConfig.java
+++ src/main/java/com/wjh/demo/activity/AppConfig.java
@@ -26,19 +26,16 @@ public class AppConfig {
         InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
 
         String[][] usersGroupsAndRoles = {
-                {"小明", "password", "ROLE_ACTIVITI_USER", "GROUP_student"},
-                {"班长", "password", "ROLE_ACTIVITI_USER", "GROUP_class_monitor"},
-                {"班主任", "password", "ROLE_ACTIVITI_USER", "GROUP_class_adviser"},
-                {"年级主任", "password", "ROLE_ACTIVITI_USER", "GROUP_grade_director"},
-                {"校长", "password", "ROLE_ACTIVITI_USER", "GROUP_schoolmaster"},
-                {"test", "123qwe", "ROLE_ACTIVITI_USER", "GROUP_other"},
-                {"admin", "password", "ROLE_ACTIVITI_ADMIN"},
+                {"clement", "123qwe", "ROLE_ACTIVITI_USER"},
+                {"leo", "123qwe", "ROLE_ACTIVITI_USER", "GROUP_PM"},
+                {"tank", "123qwe", "ROLE_ACTIVITI_USER", "GROUP_DM"},
+                {"test", "123qwe", "ROLE_ACTIVITI_USER"},
+                {"admin", "admin", "ROLE_ACTIVITI_ADMIN"},
         };
 
         for (String[] user : usersGroupsAndRoles) {
             List<String> authoritiesStrings = Arrays.asList(Arrays.copyOfRange(user, 2, user.length));
-            logger.info("> Registering new user: {} with the following Authorities [{}]"
-                    + ", password [{}]", user[0], authoritiesStrings, passwordEncoder().encode(user[1]));
+            logger.info("> Registering new user: {} with the following Authorities [{}]", user[0], authoritiesStrings);
             inMemoryUserDetailsManager.createUser(new User(user[0], passwordEncoder().encode(user[1]),
                     authoritiesStrings.stream().map(s -> new SimpleGrantedAuthority(s)).collect(Collectors.toList())));
         }
--- src/main/java/com/wjh/demo/activity/controller/ApiController.java
+++ src/main/java/com/wjh/demo/activity/controller/ApiController.java
@@ -1,39 +1,110 @@
 package com.wjh.demo.activity.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.annotation.security.RolesAllowed;
 
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.util.json.JSONArray;
+import org.activiti.engine.impl.util.json.JSONObject;
 import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RolesAllowed("ROLE_ACTIVITI_USER")
 @RestController
 public class ApiController {
     @Autowired
     private RepositoryService repositoryService;
     @Autowired
     private RuntimeService runtimeService;
-    
+    @Autowired
+    private TaskService taskService;
+
     @GetMapping("/processDefines")
-    @RolesAllowed("ROLE_ACTIVITI_ADMIN")
-    public List<String> getProcessDefines() {
+    public String getProcessDefines() {
         List<ProcessDefinition> list = repositoryService.createProcessDefinitionQuery().list();
-        List<String> names = new ArrayList<String>();
-        list.forEach(processDefine -> {
-            names.add(processDefine.getName());
+        JSONArray proDefines = new JSONArray();
+        list.forEach(item -> {
+            JSONObject proDefine = new JSONObject();
+            proDefine.put("id", item.getId());
+            proDefine.put("name", item.getName());
+            proDefine.put("key", item.getKey());
+            proDefine.put("description", item.getDescription());
+            proDefine.put("version", item.getVersion());
+            proDefines.put(proDefine);
         });
-        return names;
+        return proDefines.toString();
     }
 
-    @PostMapping("/startProcess/:id")
-    public void createProcess(@PathVariable("id") String id) {
-        runtimeService.startProcessInstanceById(id);
+    @GetMapping("/startProcess/{key}")
+    public void createProcess(@PathVariable("key") String key) {
+        ProcessInstance instance = runtimeService.startProcessInstanceByKey(key);
+        System.out.println(instance);
+    }
+
+    @GetMapping("/tasks/unAssignee")
+    public String getTasksUnAssignee() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        List<String> groups = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority()).collect(Collectors.toList());
+        System.out.println(groups);
+        List<Task> tasks = taskService.createTaskQuery().taskInvolvedGroupsIn(groups).list();
+        JSONArray taskArray = new JSONArray();
+        tasks.forEach(item -> {
+            JSONObject taskJson = new JSONObject();
+            taskJson.put("id", item.getId());
+            taskJson.put("name", item.getName());
+            taskJson.put("description", item.getDescription());
+            taskJson.put("owner", item.getOwner());
+            taskJson.put("assignee", item.getAssignee());
+            taskJson.put("ClaimTime", item.getClaimTime());
+            taskArray.put(taskJson);
+        });
+        return taskArray.toString();
+    }
+
+    @GetMapping("/task/{id}/claim")
+    public void claimTask(@PathVariable("id") String taskId) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        taskService.claim(taskId, userDetails.getUsername());
+    }
+
+    @GetMapping("/tasks")
+    public String getTasks() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        String userName = userDetails.getUsername();
+        List<Task> tasks = taskService.createTaskQuery().taskAssignee(userName).list();
+        JSONArray taskArray = new JSONArray();
+        tasks.forEach(item -> {
+            JSONObject taskJson = new JSONObject();
+            taskJson.put("id", item.getId());
+            taskJson.put("name", item.getName());
+            taskJson.put("description", item.getDescription());
+            taskJson.put("owner", item.getOwner());
+            taskJson.put("assignee", item.getAssignee());
+            taskJson.put("ClaimTime", item.getClaimTime());
+            taskArray.put(taskJson);
+        });
+        return taskArray.toString();
+    }
+
+    @GetMapping("/task/{id}/complete")
+    public void completeTask(@PathVariable("id") String taskId) {
+        Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
+        taskService.addComment(taskId, task.getProcessInstanceId(), "批准");
+        taskService.complete(taskId);
     }
 }
